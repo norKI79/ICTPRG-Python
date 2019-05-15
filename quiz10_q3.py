e5dcd020d99bfee266484c551cf734ce23051a66
norKI79 <norchis79@gmail.com>
2019-05-15 10:12:26 +1000

    quiz10_q4

---
+++ quiz10_q3.py
@@ -0,0 +1,13 @@
+def SortWordsAlphabetically(seq_x):
+    seq_x = seq_x.lower()
+    lst = seq_x.split("-")
+    lst.sort()
+    
+    return '-'.join(lst)
+
+# END
+# -------------------------------------
+# DO NOT TOUCH ANY CODE BELOW THIS LINE
+print("Test 1 Passed: " + str(SortWordsAlphabetically("Bob-does-not-like-frank") == 'bob-does-frank-like-not'))
+print("Test 2 Passed: " + str(SortWordsAlphabetically("why-am-i-doing-this-this-is-terrible") == "am-doing-i-is-terrible-this-this-why"))
+print("Test 3 Passed: " + str(SortWordsAlphabetically("frank-kill-zoe-did") == "did-frank-kill-zoe"))
---
+++ quiz10_q4.py
@@ -0,0 +1,41 @@
+def PrintBoxByWidth(wd):
+    #print out 4 lines of width wd
+
+#line 1
+    for x in range(0,wd):
+        print("x", end='')
+    
+#line 2
+    print()
+    for x in range(0,wd):
+        if(x==0):
+            print("x", end='')
+        elif(x==wd-1):
+            print("x", end='')
+        else:   
+            print(" ",end='')
+
+#line 3
+    print()
+    for x in range(0,wd):
+        if(x==0):
+            print("x", end='')
+        elif(x==wd-1):
+            print("x", end='')
+        else:   
+            print("o",end='')
+#line 4
+    print()
+    for x in range(0,wd):
+        if(x==0):
+            print("x", end='')
+        elif(x==wd-1):
+            print("x", end='')
+        else:   
+            print(" ",end='')
+#line 5
+    print()
+    for x in range(0,wd):
+        print("x", end='')
+        
+PrintBoxByWidth(20)
